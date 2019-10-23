751ed0c2c65df38e7988ed65d0f11be49ad6be2d
pierzbarry <pmbarry@asu.edu>
2019-10-23 15:46:40 -0400

    new commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+//**********
+//**********
+//**********
+
+public class Main {
+
+    public static void main(String[] args) {
+    String star = "*";
+
+    for(int i = 1; i <= 4; i++) {
+        for (int x = 1; x <= 10; x++){
+            System.out.print(star);
+        }
+        System.out.println();
+    }
+    }
+}
